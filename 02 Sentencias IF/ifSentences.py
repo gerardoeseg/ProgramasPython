cd382e6c4a4e48f27383cfe0198bac2a71901832
gerard6131@gmail.com <gerard6131@gmail.com>
2017-09-23 18:07:06 -0500

    División de temas, sentencias IF

---
+++ 02 Sentencias IF/ifSentences.py
@@ -0,0 +1,15 @@
+# Showing the uses of the if sentences
+# Conditional if
+# Extracted from: https://goo.gl/ZA4DRh
+
+number = int(raw_input("Write an integer, please: "))
+
+if number < 0:
+    number = 0
+    print 'Negative number changed to zero'
+elif number == 0:
+    print 'Number is zero'
+elif number == 1:
+    print 'Single number'
+else:
+    print 'Greater than one'    
