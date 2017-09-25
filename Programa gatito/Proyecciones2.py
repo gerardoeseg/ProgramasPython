b2be4f70f45e089ce65c09eff377d67b2c00e631
gerard6131@gmail.com <gerard6131@gmail.com>
2017-09-24 20:15:23 -0500

    Programa gatito completado

--- Programa gatito/Proyecciones2.py
+++ Programa gatito/Proyecciones2.py
@@ -1,5 +1,6 @@
+# Programa que a partir de un archivo obtiene sus valores y los pasa a una variable
+# Se opera con las variables y se imprimen los valores en un archivo 
 import math
-import pprint
 
 def sen(grados):
     return math.sin(math.radians(grados))
@@ -8,8 +9,6 @@ def cos(grados):
 print ("Proyecciones Cartograficas\n")
 
 datos = []
-x = []
-y = []
 
 with open("LongitudLatitud.txt") as archivo:
     for line in archivo:
@@ -17,13 +16,32 @@ with open("LongitudLatitud.txt") as archivo:
         # Separa cada elemento de acuerdo al separador con 'split' ('\t')
         datos.append([float(n) for n in line.strip().split('\t')])
 
-    # Divide los elementos y los asigna a una variable, los trata como numeros
-    for pair in datos:
-        try:
-            x, y = pair[0], pair[1]
-            # Se realiza cualquier operacion
-            print "El valor de x es: %f\tEl valor de y es: %f" % (x, y)
-        except IndexError:
-            print "No hay suficientes entradas."
+    with open("Resultado.txt", "w+") as resultado:
+        # Divide los elementos y los asigna a una variable, los trata como numeros
+        for pair in datos:
+            try:
+                # Los valores leidos del archivo tienen su 'x' y 'y' correspondiente
+                x, y = pair[0], pair[1]
+
+                # Se realiza cualquier operacion
+                formula = x + y
+
+                # Se imprime la longitud, latitud y el resultado de la formula
+                # '%.5f' indica que solo va a imprimir el valor con 5 decimales
+                print "Longitud: %.5f\tLatitud: %.5f\tResultado: %.5f" % (x, y, formula)
+
+                # Imprime en archivo
+                resultado.write("Longitud: ")
+                resultado.write("%.5f" % x)
+                resultado.write("\tLatitud: ")
+                resultado.write("%.5f" % y)
+                resultado.write("\tResultado: ")
+                resultado.write("%.5f\n" % formula)
+
+            # En caso de error imprime un mensaje
+            except IndexError:
+                print "No hay suficientes entradas."
+    # Cierra archivo de resultados
+    resultado.close()
 # Se cierra el archivo
 archivo.close()
